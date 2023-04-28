9a9098c0d456a2b74794d9260d09ea65930f39ce
Ana Filipa Magalhães Marques (CAA) <ana.magalhaes.marques@fidelidade.pt>
2023-04-28 16:30:40 +0100

    change path

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,17 +1,11 @@
 """cleaning data"""
-#import pathlib
 import argparse
 import pandas as pd
-
-
-#PROJECT_DIR=pathlib.Path(__file__)
 
 def clean_data(
     reg="PT",
     data=(
-        r"""C:\Users\F003482\OneDrive - Fidelidade Group\Deskto\
-        python_academy\ana_python\assignments\life_expectancy\
-        data\eu_life_expectancy_raw.tsv"""
+        r"life_expectancy\data\eu_life_expectancy_raw.tsv"
     )
     ):
     """cleaning data"""
@@ -25,14 +19,11 @@ def clean_data(
     df_unpivot['value'] = df_unpivot['value'].map(lambda x: x.lstrip('').rstrip(' epb'))
     df_unpivot=df_unpivot[df_unpivot['region']==reg]
     df_unpivot["value"]=df_unpivot["value"].astype(float)
-    df_unpivot.to_csv(r"""C:\Users\F003482\OneDrive - Fidelidade Group\Desktop\
-    python_academy\ana_python\assignments\life_expectancy\data\
-    pt_life_expectancy.csv""", index=False)
+    df_unpivot.to_csv(r"life_expectancy\data\pt_life_expectancy.csv", index=False)
     return df_unpivot
 
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-r", "--region", type=str, default="PT")
 
     args = parser.parse_args()
