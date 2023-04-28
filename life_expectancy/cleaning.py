445d1a7c8a6534057adadecc8870c903bf14f915
Ana Filipa Magalhães Marques (CAA) <ana.magalhaes.marques@fidelidade.pt>
2023-04-28 16:45:07 +0100

    Update .yml

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -5,7 +5,7 @@ import pandas as pd
 def clean_data(
     reg="PT",
     data=(
-        r"life_expectancy\data\eu_life_expectancy_raw.tsv"
+        r"data\eu_life_expectancy_raw.tsv"
     )
     ):
     """cleaning data"""
@@ -19,7 +19,7 @@ def clean_data(
     df_unpivot['value'] = df_unpivot['value'].map(lambda x: x.lstrip('').rstrip(' epb'))
     df_unpivot=df_unpivot[df_unpivot['region']==reg]
     df_unpivot["value"]=df_unpivot["value"].astype(float)
-    df_unpivot.to_csv(r"life_expectancy\data\pt_life_expectancy.csv", index=False)
+    df_unpivot.to_csv(r"data\pt_life_expectancy.csv", index=False)
     return df_unpivot
 
 if __name__ == "__main__":  # pragma: no cover
